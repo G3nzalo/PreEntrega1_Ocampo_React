9c4da179d73b5c6fffc083b1e5474dab5bfc0ae8
Gonzalo Ocampo <gonzalo.composer@gmail.com>
2023-11-21 20:46:30 -0300

    Segunda Pre entrega

---
+++ src/asyncMock.js
@@ -0,0 +1,52 @@
+const products = [
+  {
+    id: "1",
+    name: "Iphone 12",
+    price: 1800,
+    category: "celular",
+    img: "https://i.blogs.es/be13bf/1366_2000/500_500.jpeg",
+    stock: 25,
+    description: "Descripción",
+  },
+  {
+    id: "3",
+    name: "Samsung 12",
+    price: 800,
+    category: "celular",
+    img: "https://i.blogs.es/be13bf/1366_2000/500_500.jpeg",
+    stock: 10,
+    description: "Descripción",
+  },
+  {
+    id: "3",
+    name: "Nokia 12",
+    price: 400,
+    category: "celular",
+    img: "https://i.blogs.es/be13bf/1366_2000/500_500.jpeg",
+    stock: 10,
+    description: "Descripción",
+  },
+];
+
+export const getProducts = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(products);
+    }, 500);
+  });
+};
+export const getProductById = (productId) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(products.find((prod) => prod.id === productId));
+    }, 500);
+  })
+}
+
+export const getProductsByCategory = () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(products.filter((prod) => prod.category));
+      }, 500);
+    });
+  };
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,30 @@
+import { useState , useEffect } from "react";
+import {getProducts , getProductsByCategory} from '../../asyncMock'
+import ItemList from '../ItemList/ItemList'
 import "./ItenListStyle.css"
+import { useParams } from "react-router-dom";
+
 const ItemListContainer = ({greeting}) =>
 {
+    const [products , setProducts] = useState([])
+    const {categoryId} = useParams()
+
+    useEffect(()=>
+    {
+        const asyncFunc = categoryId ? getProductsByCategory : getProducts;
+
+        asyncFunc(categoryId)
+        .then(response=>{
+            setProducts(response)
+        }).catch(error => {
+            console.error(error)
+        })
+    }, [categoryId])
+    
     return (
         <div>
             <h1 className = "my-greeting">{greeting}</h1>
+            <ItemList products = {products}/>
         </div>
     )
 }
